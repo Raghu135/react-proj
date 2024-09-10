90c90a2d9084788f8340f04ef9ec91eb66f34a01
adish muhammed <adishmuhammedmm@gmail.com>
2024-09-10 12:57:52 +0530

    Updated package files, added new images, and modified several components

--- src/components/home/BrowseBooks.jsx
+++ src/components/home/BrowseBooks.jsx
@@ -1,21 +1,27 @@
 import React from "react";
-import './Browsbooks.scss';
+import "./Browsbooks.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShippingFast } from "@fortawesome/free-solid-svg-icons";
 
 function BrowseBooks() {
   return (
     <div className="home">
-      <div className="section_one">
-        <h4 className="homeintro">Introduction</h4>
-        <p>hcdsbc cksahbc jkcbsh dkbash wdkwjbd wkjdbkbd</p>
-        <div className="icon_section">
-          
+      <div className="homeone">
+        <div className="section_one">
+          <h4 className="homeintro">Introduction</h4>
+          <p>hcdsbc cksahbc jkcbsh dkbash wdkwjbd wkjdbkbd</p>
+          <div className="icon_section">
+            <FontAwesomeIcon icon={faShippingFast} />
+            <FontAwesomeIcon icon={faShippingFast} />
+            <FontAwesomeIcon icon={faShippingFast} />
+            <FontAwesomeIcon icon={faShippingFast} />
+          </div>
+        </div>
+        <div className="image_section">
+          <img src="/img/book1.jpeg" alt="Book" />
         </div>
       </div>
-      <div className="image_section">
-        <img src="/img/book1.jpeg" alt="Book"></img>
-      </div>
     </div>
-
   );
 }
 
